db7ff77bd5f29777cc3d1d5cd825011b3eda05cc
dajvido <dawid.jaskot@gmail.com>
2015-05-07 15:02:21 +0200

    Add fields to User model

---
+++ db/migrate/20150507113846_add_firstname_to_user.rb
@@ -0,0 +1,5 @@
+class AddFirstnameToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :firstname, :string, null: false
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  it { should validate_presence_of :firstname }
-  it { should validate_presence_of :lastname }
+  describe 'validations' do
+    it { should validate_presence_of :firstname }
+    it { should validate_presence_of :lastname }
+
+  end
 
   it "by default isn't admin" do
     expect(User.new).to_not be_admin
